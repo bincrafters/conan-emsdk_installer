14bbf17e5fe8d5cbb32251c3f5d4d2c71e800d38
SSE4 <tomskside@gmail.com>
2018-05-12 01:32:42 +0700

    - reduce CI configurations
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- build.py
+++ build.py
@@ -3,9 +3,34 @@
 
 
 from bincrafters import build_template_default
+import platform
+import os
+
 
 if __name__ == "__main__":
 
     builder = build_template_default.get_builder()
+    builder.builds = []
+
+    settings = dict()
+    settings['compiler'] = 'clang'
+    settings['compiler.version'] = '5.0'
+    settings['compiler.libcxx'] = 'libc++'
+
+    if platform.system() == 'Windows':
+        settings['os_build'] = 'Windows'
+    elif platform.system() == 'Linux':
+        settings['os_build'] = 'Linux'
+    elif platofmr.system() == 'Darwin':
+        settings['os_build'] = 'Macos'
+
+    if 'ARCH_BUILD' in os.environ:
+        arches_build = [os.environ['ARCH_BUILD']]
+    else:
+        arches_build = ['x86', 'x86_64']
+
+    for arch_build in arches_build:
+        settings['arch_build'] = arch_build
+        builder.add(settings=settings.copy(), options={}, env_vars={}, build_requires={})
 
     builder.run()
