1669be9926940e0afb8a85b01d7f3d3dcbe6eea3
Florian Stellbrink <flo@stellbr.ink>
2020-01-27 00:55:00 +0100

    Change build architecture back to 64bit
    That wasn't necessary., and breaks mac os builds since they don't have pre-compiled node x86 builds.

--- conanfile.py
+++ conanfile.py
@@ -14,7 +14,7 @@ class EmSDKInstallerConan(ConanFile):
 
     settings = {
         "os_build": ['Windows', 'Linux', 'Macos'],
-        "arch_build": ['x86'],
+        "arch_build": ['x86_64'],
         "arch": ['x86'],
     }
     short_paths = True
